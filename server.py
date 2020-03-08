17d81786da2ce7fb60717ce26c49dde1f40172ab
Dominik Dancs <dodancs@moow.info>
2020-03-08 15:03:30 +0100

    Pridany balik na websockety + nacitanie konfiguracie zo suboru

--- server.py
+++ server.py
@@ -3,14 +3,32 @@ from flask_jwt_extended import (
     JWTManager, jwt_required, create_access_token,
     get_jwt_identity
 )
+from flask_socketio import SocketIO, send, emit
+import uuid
+import json
+
+with open('config.json') as config_file:
+    config = json.load(config_file)
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = config['secret']
+app.config['JWT_SECRET_KEY'] = config['jwt_secret']
+jwt = JWTManager(app)
+socketio = SocketIO(app)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+
+# @socketio.on('connect')
+# def websocketTest():
+#     emit('my response', {'data': 'Connected'})
+
+
+if __name__ == '__main__':
+    socketio.run(app)
 
 # if __name__ == '__main__':
 #     app.run()
