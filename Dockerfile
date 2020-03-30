bed5a67ce1651ae0e67050edb7eda47c896c7daf
Dominik Dancs <dodancs@moow.info>
2020-03-30 14:20:41 +0200

    pridane otvorenie portu pre docker container

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM python:3.7
 
+EXPOSE 5000/tcp
+
 WORKDIR ./
 
 COPY requirements.txt ./
--- server.py
+++ server.py
@@ -69,4 +69,4 @@ import module_donations
 if __name__ == '__main__':
     # if the server runs unreliably, uncomment the first line, and comment the line with socketio
     # app.run(host='0.0.0.0')
-    socketio.run(app, host='0.0.0.0', debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
